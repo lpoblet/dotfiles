82c94e64124334718d2d0d1b532fdd88530a2c51
Leandro Poblet <leandropoblet@gmail.com>
2022-03-11 23:14:34 +1300

    removed unnecessary bin folder

--- bin/bin/joplin.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-
-#NOSANDBOX=$($HOME/Applications/Joplin_378f0ac1a8497ba7bc14001499de1814.AppImage --no-sandbox)
-NOSANDBOX=$($HOME/Applications/Joplin_eb68d86a4fe7320765757a8d04862b4a.AppImage --no-sandbox)
-
